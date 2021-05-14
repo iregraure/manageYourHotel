798201d75dcd9ed3459b0495c22d03500d51754e
iregraure <iregraure@gmail.com>
2021-05-14 08:32:55 +0200

    Added ids in HTML to make Selenium tests

--- Frontend/src/app/modules/hotel/hotel.module.ts
+++ Frontend/src/app/modules/hotel/hotel.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
 import { HotelRoutingModule } from './hotel-routing.module';
+
+// Components
 import { HotelComponent } from './hotel.component';
 import { MainHotelComponent } from './components/main-hotel/main-hotel.component';
 import { ClientsManagementComponent } from './components/clients-management/clients-management.component';
 import { NewClientComponent } from './components/new-client/new-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
 
 // Material imports
 import { MatIconModule } from '@angular/material/icon';
@@ -14,12 +16,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 // Other imports
 import { NgxPaginationModule } from 'ngx-pagination';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
 
 
 @NgModule({
@@ -34,7 +34,6 @@ import { ClientDetailsComponent } from './components/client-details/client-detai
     MatInputModule,
     MatTableModule,
     MatCardModule,
-    MatPaginatorModule,
     NgxPaginationModule,
     MatSortModule
   ]
