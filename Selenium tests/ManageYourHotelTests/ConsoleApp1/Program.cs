4e8af8197060680728f00137c28fd4b264959370
iregraure <iregraure@gmail.com>
2021-06-11 22:02:13 +0200

    Ultimos retoques al código antes de entregar el proyecto

--- Selenium tests/ManageYourHotelTests/ConsoleApp1/Program.cs
+++ Selenium tests/ManageYourHotelTests/ConsoleApp1/Program.cs
@@ -60,62 +60,66 @@ namespace ManageYourHotelTests
             newClientButton.Click();
             // Fill the form and create de client
             Thread.Sleep(2000);
-            IWebElement nameInput = driver.FindElement(By.Id("name"));
-            IWebElement surnameInput = driver.FindElement(By.Id("surname"));
-            IWebElement dniInput = driver.FindElement(By.Id("dni"));
-            IWebElement emailInput = driver.FindElement(By.Id("email"));
-            IWebElement addressInput = driver.FindElement(By.Id("address"));
-            IWebElement phoneInput = driver.FindElement(By.Id("phone"));
-            IWebElement birthdateInput = driver.FindElement(By.Id("birthDate"));
-            IWebElement createButton = driver.FindElement(By.Id("create"));
-            nameInput.SendKeys("Selenium");
-            surnameInput.SendKeys("Automated");
-            dniInput.SendKeys("11122233E");
-            emailInput.SendKeys("seleniumautomated@gmail.com");
-            addressInput.SendKeys("5th Selenium street");
-            phoneInput.SendKeys("666999888");
-            birthdateInput.SendKeys("16/06/1985");
-            Thread.Sleep(2000);
-            createButton.Click();
-            // Go to room management
-            IWebElement goHome = driver.FindElement(By.Id("goHome"));
-            goHome.Click();
-            IWebElement rooms = driver.FindElement(By.Id("roomManagement"));
-            rooms.Click();
-            Thread.Sleep(2000);
-            // Search for the room and see its details
-            IWebElement building = driver.FindElement(By.Id("Building2"));
-            building.Click();
-            Thread.Sleep(2000);
-            IWebElement floor = driver.FindElement(By.Id("2"));
-            floor.Click();
-            Thread.Sleep(2000);
-            IWebElement room = driver.FindElement(By.Id("201"));
-            room.Click();
-            Thread.Sleep(2000);
-            // Click on the "Add stay" button and fill the form to add a stay for the new client
-            IWebElement addStayButton = driver.FindElement(By.Id("addStay"));
-            addStayButton.Click();
-            Thread.Sleep(2000);
-            IWebElement startDate = driver.FindElement(By.Id("startDate"));
-            IWebElement endDate = driver.FindElement(By.Id("endDate"));
-            IWebElement clientDni = driver.FindElement(By.Id("clientDni"));
-            IWebElement addButton = driver.FindElement(By.Id("addStay"));
-            startDate.SendKeys("01/07/2021");
-            endDate.SendKeys("07/07/2021");
-            clientDni.SendKeys("11122233E");
-            Thread.Sleep(2000);
-            addButton.Click();
+            //IWebElement nameInput = driver.FindElement(By.Id("name"));
+            //IWebElement surnameInput = driver.FindElement(By.Id("surname"));
+            //IWebElement dniInput = driver.FindElement(By.Id("dni"));
+            //IWebElement emailInput = driver.FindElement(By.Id("email"));
+            //IWebElement addressInput = driver.FindElement(By.Id("address"));
+            //IWebElement phoneInput = driver.FindElement(By.Id("phone"));
+            //IWebElement birthdateInput = driver.FindElement(By.Id("birthDate"));
+            //IWebElement createButton = driver.FindElement(By.Id("create"));
+            //nameInput.SendKeys("Selenium");
+            //surnameInput.SendKeys("Automated");
+            //dniInput.SendKeys("11122233E");
+            //emailInput.SendKeys("seleniumautomated@gmail.com");
+            //addressInput.SendKeys("5th Selenium street");
+            //phoneInput.SendKeys("666999888");
+            //birthdateInput.SendKeys("16/06/1985");
+            //Thread.Sleep(2000);
+            //createButton.Click();
+            //Thread.Sleep(2000);
+            //// Go to room management
+            //IWebElement goHome = driver.FindElement(By.Id("goHome"));
+            //goHome.Click();
+            //IWebElement rooms = driver.FindElement(By.Id("roomManagement"));
+            //rooms.Click();
+            //Thread.Sleep(2000);
+            //// Search for the room and see its details
+            //IWebElement building = driver.FindElement(By.Id("Building2"));
+            //building.Click();
+            //Thread.Sleep(2000);
+            //IWebElement floor = driver.FindElement(By.Id("2"));
+            //floor.Click();
+            //Thread.Sleep(2000);
+            //IWebElement room = driver.FindElement(By.Id("201"));
+            //room.Click();
+            //Thread.Sleep(2000);
+            //// Click on the "Add stay" button and fill the form to add a stay for the new client
+            //IWebElement addStayButton = driver.FindElement(By.Id("addStay"));
+            //addStayButton.Click();
+            //Thread.Sleep(2000);
+            //IWebElement startDate = driver.FindElement(By.Id("startDate"));
+            //IWebElement endDate = driver.FindElement(By.Id("endDate"));
+            //IWebElement clientDni = driver.FindElement(By.Id("clientDni"));
+            //IWebElement addButton = driver.FindElement(By.Id("addStay"));
+            //startDate.SendKeys("01/07/2021");
+            //endDate.SendKeys("07/07/2021");
+            //clientDni.SendKeys("11122233E");
+            //Thread.Sleep(2000);
+            //addButton.Click();
             // Log out of the app
             IWebElement logOut = driver.FindElement(By.Id("logOut"));
             logOut.Click();
+            Thread.Sleep(2000);
+            IWebElement confirm = driver.FindElement(By.Id("acceptConfirm"));
+            confirm.Click();
             Thread.Sleep(2000);
         }
 
         private static void clientApp(IWebDriver driver)
         {
             // Login into the app
-            login(driver, "11122233E", "Selenium");
+            login(driver, "11122233E", "11122233E");
             Thread.Sleep(2000);
             // Access the user management and change the user's password
             IWebElement user = driver.FindElement(By.Id("userManagement"));
